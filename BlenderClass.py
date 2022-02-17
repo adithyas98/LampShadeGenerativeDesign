c575c0cf2a89fe5341baf44dc955d2ea5cb2c186
sakib15499 <sakib15499@gmail.com>
2022-02-17 18:40:48 -0500

    wrote methods for cube and cylinder
    there is some error that needs to be fixed

---
+++ BlenderClass.py
@@ -0,0 +1,52 @@
+
+import bpy #Python module for Blender
+
+class Blender:
+    '''
+    This class will implement all the necessary Blender methods
+    '''
+    
+    def __init__(self):
+        self.debug = True #simple debug variable
+            
+    def cube(self, size, x, y, z, rx, ry, rz, sx, sy, sz):
+        '''
+        This will create a cube with desired inputs
+        Inputs:
+            - size: size of the cube
+            - x: location on x axis
+            - y: location on y axis
+            - z: location on z axis
+            - rx: rotation about x
+            - ry: rotation about y
+            - rz: rotation about z
+            - sx: scale factor along x
+            - sy: scale factor along y
+            - sz: scale factor along z
+        Output:
+            - creates a cube with desired parameters
+        '''
+        bpy.ops.mesh.primitive_cube_add(size=size, align='WORLD', \
+                                        location=(x, y, z), rotation=(rx, ry, rz), scale=(sx, sy, sz))
+            
+    def cylinder(self, v, r, d, x, y, z, rx, ry, rz, sx, sy, sz):
+        '''
+        This will create a cylinder with desired inputs
+        Inputs:
+            - v: vertices used to build cylinder
+            - r: radius of cylinder
+            - d: depth of cylinder
+            - x: location on x axis
+            - y: location on y axis
+            - z: location on z axis
+            - rx: rotation about x
+            - ry: rotation about y
+            - rz: rotation about z
+            - sx: scale factor along x
+            - sy: scale factor along y
+            - sz: scale factor along z
+        Output:
+            - creates a cylinder with desired parameters
+        '''
+        bpy.ops.mesh.primitive_cylinder_add(vertices=v, radius=r, depth=d, \
+                                            end_fill_type='NGON', align='WORLD', location=(x, y, z), rotation=(rx, ry, rz), scale=(sx, sy, sz))            
